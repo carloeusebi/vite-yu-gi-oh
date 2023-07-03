3bccd47073e3c5302d15722db62b6a70ccfd6f7c
carloeusebi <carloeusebi@gmail.com>
2023-07-03 18:11:12 +0200

    Completed header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <script>
 import axios from 'axios';
 import { store } from './assets/store';
+import AppHeader from './components/AppHeader.vue';
+import AppContainer from './components/AppContainer.vue';
 
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
 
-axios.get(endpoint).then(res => {
-	console.log(res.data);
-	store.pokemons.list = res.data.docs;
-	store.pokemons.pages.prev = res.data.prevPage;
-	store.pokemons.pages.next = res.data.nextPage;
-	store.pokemons.pages.current = res.data.page;
-});
+const fetchPokemons = page => {
+	axios
+		.get(`${endpoint}`)
+		.then(res => {
+			console.log(res.data);
+			store.pokemons.list = res.data.docs;
+			store.pokemons.pages.prev = res.data.prevPage;
+			store.pokemons.pages.next = res.data.nextPage;
+			store.pokemons.pages.current = res.data.page;
+		})
+		.catch(e => {
+			console.error(e);
+		});
+};
+export default {
+	data() {
+		return {
+			store,
+		};
+	},
+
+	components: { AppContainer, AppHeader },
+
+	mounted() {
+		fetchPokemons();
+	},
+};
 </script>
 
 <template>
-	<h1>hello</h1>
+	<AppHeader />
+	<AppContainer />
 </template>
 
-<style scoped></style>
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {};
+</script>
+
+<template>
+	<header class="container text-center my-3">
+		<img
+			class="logo"
+			src="pokemon.png"
+			alt="logo" />
+	</header>
+</template>
+
+<style scoped>
+.logo {
+	height: 100px;
+}
+</style>
